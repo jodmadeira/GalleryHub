e0b97a21aaf8a0b1797ff6aa5e31b523b689f3d5
Joao Madeira <129863118+jodmadeira@users.noreply.github.com>
2023-05-15 10:06:12 +0100

    final dependencies

--- GalleryHub/server.js
+++ GalleryHub/server.js
@@ -4,5 +4,5 @@ const app = require("./app");
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server listening on http://127.0.0.l:${PORT}`);
+  console.log(`Server listening on http://127.0.0.1:${PORT}`);
 });

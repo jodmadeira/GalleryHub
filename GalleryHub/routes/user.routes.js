789df01ac0563403cdbf50caed00877e81ffd20a
Joao Madeira <129863118+jodmadeira@users.noreply.github.com>
2023-05-16 14:40:54 +0100

    changes to user pages

--- GalleryHub/routes/user.routes.js
+++ GalleryHub/routes/user.routes.js
@@ -9,14 +9,20 @@ const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/GalleryH
 const User = require("../models/User.model");
 
 // GET /profile/
-router.get("/profile/:id", (req, res) => {
-    const {id} = req.params.id
-    console.log('req.session',req.session)
+router.get("/profile", (req, res) => {
+    const userId = req.session.currentUser._id
+    
+    
+    console.log({userId});
+     
+    //console.log(userId);
+    //console.log('REQ.PARAMS',req.params);
+    //console.log('req.session',req.session);
     async function getUserDetails (){
         try {
-            const user = await User.findById({id})
+            const user = await User.findById(userId)
             console.log('user',user)
-            res.render("user/user-profile", {user});
+            res.render("user/userProfile", {user});
         }
         catch(error){
             console.log(error);
@@ -30,5 +36,33 @@ router.get("/profile/:id", (req, res) => {
 });
 
 
+
+router.get("/profile/update", (req, res) => {
+    const userId = req.session.currentUser._id;
+
+    async function getUserDetails (){
+        try {
+            const user = await User.findById(userId)
+            //console.log('user',user)
+            res.render("user/userUpdate", {user});
+        }
+        catch(error){
+            console.log(error);
+            res.redirect('/profile')}}
+    getUserDetails();
+})
+
+router.post("/profile/update", (req, res) => {
+    
+    async function updateUser() {
+    try {
+        
+        res.redirect('/')       
+    } catch (error) {
+        console.log(error)        
+    }
+    };
+    updateUser();
+})
 
 module.exports = router;

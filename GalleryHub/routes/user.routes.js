650ebb187e76a3e615f2bb5a52d67e92fa1fc24a
Joao Madeira <129863118+jodmadeira@users.noreply.github.com>
2023-05-17 15:01:03 +0100

    users: collections in profile

--- GalleryHub/routes/user.routes.js
+++ GalleryHub/routes/user.routes.js
@@ -10,17 +10,27 @@ const isLoggedOut = require("../middleware/isLoggedOut");
 const isLoggedIn = require("../middleware/isLoggedIn");
 
 const User = require("../models/User.model");
+const Collection = require("../models/Collection.model");
 const fileUploader = require('../config/cloudinary.config.js');
 
 
 // GET /profile/
-router.get("/profile", isLoggedOut,(req, res) => {
+router.get("/profile", isLoggedIn,(req, res) => {
     
     async function getUserDetails (){
         try {
             const userId = req.session.currentUser._id
             const user = await User.findById(userId)
-            res.render("user/userProfile", {user});
+            const userCollectionsId = user.ownedCollections
+            let collectionTitles=[];
+            for(let i=0;i<userCollectionsId.length;i++){
+                
+                let collection = await Collection.findById(userCollectionsId[i])
+                
+                collectionTitles.push(collection.title)
+                console.log(collectionTitles)
+            }
+            res.render("user/userProfile", {user, collectionTitles});
         }
         catch(error){
             console.log(error);
@@ -32,13 +42,12 @@ router.get("/profile", isLoggedOut,(req, res) => {
 
 
 
-router.get("/profile/update", (req, res) => {
+router.get("/profile/update", isLoggedIn, (req, res) => {
     const userId = req.session.currentUser._id;
 
     async function getUserDetails (){
         try {
             const user = await User.findById(userId)
-            console.log('user',user)
             res.render("user/userUpdate", {user});
         }
         catch(error){
@@ -50,13 +59,11 @@ router.get("/profile/update", (req, res) => {
 router.post("/profile/update", fileUploader.single('imgSrc') ,(req, res) => {
     const userId = req.session.currentUser._id
     const {name, email, bio} = req.body
-    console.log(req.file);
 
     async function updateUser() {
     try {
-        console.log(userId)
         const updateUserInfo = await User.findByIdAndUpdate(userId,{name, email, imgSrc:req.file.path, bio})
-        console.log(updateUserInfo)
+        
         res.redirect('/profile')       
     } catch (error) {
         console.log(error)        
